bdd9d35ef7e35d97e5bd6b5871002ae5353125b0
Lê Văn Mạnh <74082407+Levanmanh2002@users.noreply.github.com>
2022-09-07 11:24:44 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class MyApp extends StatelessWidget {
           )),
           fontFamily: 'SF-Pro-Rounded',
         ),
-        home: const SettingsScreen(),
+        home: const App(),
       ),
     );
   }
